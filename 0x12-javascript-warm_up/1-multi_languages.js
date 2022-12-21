64c9dafd335a42794b16ef81b6d64305e10bc0f0
Miko1203744 <106764762+Miko1203744@users.noreply.github.com>
2022-12-21 07:27:34 -0600

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
-['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach(
-  elem => console.log(elem)
-);
+const texts = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < texts.length; i++) {
+  console.log(texts[i]);
+}
